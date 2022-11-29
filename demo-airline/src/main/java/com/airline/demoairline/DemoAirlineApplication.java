b1fd6c136b83832beb1526c8e36664f8b4e96981
Álvaro Urda <alvaro.urda@solera.com>
2022-11-29 09:49:04 +0100

    basic structure added

---
+++ demo-airline/src/main/java/com/airline/demoairline/DemoAirlineApplication.java
@@ -0,0 +1,13 @@
+package com.airline.demoairline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAirlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoAirlineApplication.class, args);
+	}
+
+}
---
+++ demo-airline/src/test/java/com/airline/demoairline/DemoAirlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airline.demoairline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAirlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
